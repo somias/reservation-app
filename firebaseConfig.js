03b6a6b7b4cb84256deae95e2b4713081f46f0ae
somias <milos.savkovic.somias@gmail.com>
2024-07-20 16:15:15 +0200

    Init expo

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6oDr4y67dFXHdHCO1BDkWTW8CIXEwK-o",
+  authDomain: "vejas-reservation.firebaseapp.com",
+  projectId: "vejas-reservation",
+  storageBucket: "vejas-reservation.appspot.com",
+  messagingSenderId: "199095651860",
+  appId: "1:199095651860:web:d0181196a3c86a9b336101",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
