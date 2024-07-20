7b7e706a44bc5b7c85ba19688a30c38c0c9f7dfb
somias <milos.savkovic.somias@gmail.com>
2024-07-20 16:48:17 +0200

    Add basic Date Component

---
+++ components/Date.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+type DateProps = {};
+
+const Date = (props: DateProps) => {
+  return (
+    <View>
+      <Text>Component!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Date;
